c7832f6890b57827599257cac43ed040c9b48b09
Pedro <p.miraspc35@gmail.com>
2025-09-22 18:34:49 +0200

    Agregar funcionalidad de navegación y mostrar/ocultar secciones en el script

--- DWEC/T1/EJ1/js/script.js
+++ DWEC/T1/EJ1/js/script.js
@@ -1,6 +1,25 @@
 const btnHM = document.getElementById("btnHM");
 const btnImg = document.getElementById("btnImg");
 const btnForm = document.getElementById("btnForm");
+const HW = document.getElementById("HW");
+const image = document.getElementById("image");
+const form = document.getElementById("formulario");
 
 function showText(){
+    HW.style.display = "block";
+    image.style.display = "none";
+    form.style.display = "none";
 };
+
+function showImage(){
+    btnHM.style.display = "none";
+    btnImg.style.display = "block";
+    btnForm.style.display = "none";
+}
+function showForm(){
+    btnHM.style.display = "none";
+    btnImg.style.display = "none";
+    btnForm.style.display = "block";
+}
+
+btnHM.addEventListener("click", showImage);
