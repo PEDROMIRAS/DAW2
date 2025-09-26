a567eed3ef4d627c3e0a6bb841ad93a5fbe27924
Pedro <p.miraspc35@gmail.com>
2025-09-26 17:10:27 +0200

    Mod Plantilla

---
+++ DWEC/T2/pruebaStrings/assets/JS/script.js
@@ -0,0 +1,3 @@
+let texto = "El acceso a la ruta C:\\\\usuario\\ tarda 1'23\", algo considerado \"lento\" en la actualidad.";
+const contenedor = document.getElementById("container");
+contenedor.innerHTML = `<p>${texto}</p>`;
