f9f348ac191ceb852906e809fd1ebde6fc4df1d4
Pedro <p.miraspc35@gmail.com>
2025-10-27 15:49:42 +0100

    Updates

--- DWEC/T2/ActividadesAplicacion/AA213/213.js
+++ DWEC/T2/ActividadesAplicacion/AA213/213.js
@@ -1,21 +1,35 @@
+// Solicita al usuario que escriba el primer número y lo guarda en la variable op1
 const op1 = prompt('Escribe el primer número');
+
+// Solicita al usuario que escriba el símbolo de la operación (+, -, *, /, %, **) y lo guarda en la variable simbolo
 const simbolo = prompt('Escribe el símbolo');
+
+// Solicita al usuario que escriba el segundo número y lo guarda en la variable op2
 const op2 = prompt('Escribe el segundo número');
 
+// Se declara la variable resultado, que almacenará el resultado de la operación
 let resultado;
 
-if(simbolo === '+'){
+// Se comprueba qué símbolo ha introducido el usuario y se realiza la operación correspondiente
+if (simbolo === '+') {
+    // Suma de los dos números
     resultado = Number(op1) + Number(op2);
-}else if(simbolo === '-'){
+} else if (simbolo === '-') {
+    // Resta de los dos números
     resultado = Number(op1) - Number(op2);
-}else if(simbolo === '*'){
+} else if (simbolo === '*') {
+    // Multiplicación de los dos números
     resultado = Number(op1) * Number(op2);
-}else if(simbolo === '/'){
+} else if (simbolo === '/') {
+    // División de los dos números
     resultado = Number(op1) / Number(op2);
-}else if(simbolo === '%'){
+} else if (simbolo === '%') {
+    // Módulo: el resto de dividir op1 entre op2
     resultado = Number(op1) % Number(op2);
-}else if(simbolo === '**'){
+} else if (simbolo === '**') {
+    // Potencia: op1 elevado a op2
     resultado = Number(op1) ** Number(op2);
 }
 
+// Muestra el resultado en la consola
 console.log(`Resultado: ${resultado}`);
---
+++ DWEC/T4/AP43/ap43.js
@@ -0,0 +1,19 @@
+//Declaramos una palabra clave como variable
+const palabraClave = "murcielago";
+
+//Definimos la funcion
+function palabreja(...letras) {
+    let resultado = "";
+    //Recorremos la palabra clave
+    for (let letra of palabraClave) {
+        if (letras.includes(letra)) {//Comprobamos si la palabra clave contiene alguna de las letras insertadas 
+            resultado += letra; // si se adivinó la letra, la mostramos
+        } else {
+            resultado += "-";   // si no, mostramos un guion
+        }
+    }
+
+    console.log(resultado);
+}
+//Llamamos a la funcion, le indicamos unos parametros variables
+palabreja("m", "r", "l", "a", "t", "e", "o");
