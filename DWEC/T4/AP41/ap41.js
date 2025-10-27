1cf135ae8f396e3a479f06c000feec5c93f9231f
Pedro <p.miraspc35@gmail.com>
2025-10-27 15:12:43 +0100

    Update 41.js

--- DWEC/T4/AP41/ap41.js
+++ DWEC/T4/AP41/ap41.js
@@ -1,10 +1,13 @@
-
+//Solicitamos el valor de las variables al usuario.
 const precio = prompt('Introduce un precio de producto');
 const porcentaje = prompt('Introduce un porcentaje de descuento');
 
+//Definimos la funcion
 function descuento(precio,porcentaje){
     descuentoPrecio = precio-(precio*(porcentaje/100));
     return descuentoPrecio;
 }
 
+//Llamamos a la funcion en una salida por consola, pasandole como parametros
+// los valores recogidos en los prompts
 console.log(descuento(precio,porcentaje));
