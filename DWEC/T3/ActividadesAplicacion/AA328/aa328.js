dd092be44e28f9341f616ae0d2a4fe43b6f10d90
Pedro <p.miraspc35@gmail.com>
2025-11-02 17:56:00 +0100

    Ajuste comentarios

--- DWEC/T3/ActividadesAplicacion/AA328/aa328.js
+++ DWEC/T3/ActividadesAplicacion/AA328/aa328.js
@@ -1,4 +1,4 @@
-//Array feo del ejercicio
+//Array del ejercicio anterior
 let arrayMasivo = [[21,2,3,4],[45,32,22,9],[0,66,7,4],[-3,32,11,90],[59,-2,-63,15]];
 
 //Concatenamos todo el array del ej 27 en un solo array nuevo
